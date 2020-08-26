841bada084f8593082b76a6306992d9ca53d3ce0
Hugo Menzaghi <hmenzagh@student.42.fr>
2020-08-26 19:06:20 +0200

    test again

--- components/prism-lango.js
+++ components/prism-lango.js
@@ -1,13 +1,13 @@
 (function (Prism) {
 	Prism.languages.lango = {
-		whiteSpace: /[ \t\n]+/,
-		number: /\d+\.?\d*/,
-		reference: /@[a-zA-Z0-9_]+/,
-		operator: /[+\-/*%]/,
-		cast: /\([a-z]+\)/,
-		parentheses: /[\(\)]/,
-		coma: ',',
-		equalityOperator: /(=|=>|=>>)/,
-		funct: /(duree)/,
+		'whiteSpace': /[ \t\n]+/,
+		'number': /\d+\.?\d*/,
+		'reference': /@[a-zA-Z0-9_]+/,
+		'operator': /[+\-/*%]/,
+		'cast': /\([a-z]+\)/,
+		'parentheses': /[\(\)]/,
+		'coma': /(,)/,
+		'equalityOperator': /(=|=>|=>>)/,
+		'funct': /(duree)/,
 	};
 }(Prism));
